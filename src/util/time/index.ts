f5ffe65416b16c93b80b461fbb77b56bd9500434
zen0 <79776344+zen0dev@users.noreply.github.com>
2022-02-18 09:56:06 +0000

    build

--- src/util/time/index.ts
+++ src/util/time/index.ts
@@ -5,3 +5,9 @@ import _fancyTimeFormat from './fancyTimeFormat'
 export const date = _date
 export const now = _now
 export const fancyTimeFormat = _fancyTimeFormat
+
+export default {
+  date,
+  now,
+  fancyTimeFormat
+}
