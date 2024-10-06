57f2344cf1d1e17f35509a803631cc7c07dc3ce4
zen0 <79776344+zen0dev@users.noreply.github.com>
2024-10-05 22:32:22 -0600

    fix util: rpc roles

--- util/rpc.ts
+++ util/rpc.ts
@@ -164,15 +164,17 @@ export const validateRequest = (t: any) =>
 export const hasRole = (role: string | string[], t: any) =>
   t.middleware(async ({ input, ctx, next }) => {
     console.log('hasRole', role, ctx.client?.roles);
-    if (ctx.client?.roles?.length > 0 && Array.isArray(role)) {
-      const hasAnyRole = role.some((r) => ctx.client.roles.includes(r));
-      if (!hasAnyRole) {
-        return { status: 0, message: `Not authorized. Missing one of the required roles: ${role.join(',')}` };
-      }
-    } else {
-      if (!ctx.client.roles.includes(role)) {
+    if (ctx.client?.roles?.length > 0) {
+      if (Array.isArray(role)) {
+        const hasAnyRole = role.some((r) => ctx.client.roles.includes(r));
+        if (!hasAnyRole) {
+          return { status: 0, message: `Not authorized. Missing one of the required roles: ${role.join(',')}` };
+        }
+      } else if (ctx.client.roles.includes(role)) {
         return { status: 0, message: `Not authorized. Missing role: ${role}` };
       }
+    } else {
+      return { status: 0, message: `Not authorized. Missing role: ${role}` };
     }
 
     return next();
