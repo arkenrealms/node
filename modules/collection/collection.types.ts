5f7d20fc5f49b863dedee42710879ded4a439be6
horlami <akintolaolamilekan51@gmail.com>
2024-10-28 14:18:30 +0100

    removed price from schema

--- modules/collection/collection.types.ts
+++ modules/collection/collection.types.ts
@@ -13,7 +13,6 @@ export type CollectibleCollection = z.infer<typeof schema.CollectibleCollection>
 export type CollectibleCardBox = z.infer<typeof schema.CollectibleCardBox>;
 export type CollectibleCardPack = z.infer<typeof schema.CollectibleCardPack>;
 export type CollectibleCard = z.infer<typeof schema.CollectibleCard>;
-export type Price = z.infer<typeof schema.Price>;
 export type Card = z.infer<typeof schema.Card>;
 export type Set = z.infer<typeof schema.Set>;
 export type Series = z.infer<typeof schema.Series>;
@@ -22,7 +21,6 @@ export type CollectibleCollectionDocument = CollectibleCollection & Document;
 export type CollectibleCardBoxDocument = CollectibleCardBox & Document;
 export type CollectibleCardPackDocument = CollectibleCardPack & Document;
 export type CollectibleCardDocument = CollectibleCard & Document;
-export type PriceDocument = Price & Document;
 export type CardDocument = Card & Document;
 export type SetDocument = Set & Document;
 export type SeriesDocument = Series & Document;
@@ -32,7 +30,6 @@ export type Mappings = {
   CollectibleCardBox: Model<CollectibleCardBoxDocument>;
   CollectibleCardPack: Model<CollectibleCardPackDocument>;
   CollectibleCard: Model<CollectibleCardDocument>;
-  Price: Model<PriceDocument>;
   Card: Model<CardDocument>;
   Set: Model<SetDocument>;
   Series: Model<SeriesDocument>;
